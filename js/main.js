bd8af24106da709b8b7b18d7e4f6e3c313b797af
MarGallito <123398118+MarGallito@users.noreply.github.com>
2023-03-20 01:27:18 +0400

    ended

---
+++ js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// #1-#2
+let myName = prompt("თქვენი სახელი და გვარი:");
+console.log(myName);
+
+// #3
+let arr = [12, 15];
+// #4
+let sum = arr[0] + arr[1];
+// #5
+console.log(sum);
+// #6
+let obj = {
+    name: "Abel",
+    age: "21",
+};
+// #7
+obj.name = "Vaxo";
+obj.age = 33;
+console.log(obj);
+// #8
+let arrTwo = [];
+arrTwo.push(1);
+arrTwo.push(2);
+arrTwo.push(3);
+arrTwo.push(4);
+arrTwo.push(5);
+arrTwo.push(6);
+arrTwo.push(7);
+arrTwo.push(8);
+arrTwo.push(9);
+// #9
+for (let i = 0; i < arr2.length; i++) {
+    console.log(arr2[i]);
+}
+
+
+
+
